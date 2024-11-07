11d1fb591f9263ac2f597c841b226c25d16df41e
davidebaldi <d.baldi88@gmail.com>
2024-11-07 12:23:42 +0100

    combination random generated

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = Vue.createApp({
             title: "Mastermind",
             messageBtn: "Start game",
             seconds: 30,
-            timer: null // to store the timer reference
+            timer: null, // to store the timer reference,
+            numbers: [17, 18, 19, 20, 13, 14, 15, 16, 9, 10, 11, 12, 5, 6, 7, 8, 1, 2, 3, 4],
+            colorsArray: ["red","orange","yellow","green","blue","purple","pink","brown","aqua"],
+            secretCombination: []
         }
     },
     methods: {
@@ -19,6 +22,13 @@ const app = Vue.createApp({
         },
         stopTime() {
             clearInterval(this.timer); // optional method to stop the timer
+        },
+        generatedSecretCombination () {
+            for (let i = 0; i < 4; i++) {
+                let randomColourIndex = Math.floor(Math.random() * this.colorsArray.length);
+                this.secretCombination.push(this.colorsArray[randomColourIndex])
+                console.log(this.secretCombination)
+            }
         }
     },
     computed: {
